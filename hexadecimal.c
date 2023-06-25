7864e38a372d87a902338353fc03c30347714bed
Abdurrahim Yesilbas <abdyesilbas@gmail.com>
2023-06-25 15:15:10 +0300

    hexadecimal bicim

---
+++ hexadecimal.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    double ondalikSayi;
+
+    printf("Bir ondalik sayi girin: ");
+    scanf("%lf", &ondalikSayi);
+
+    printf("Girilen sayi onaltilik olarak: %a\n", ondalikSayi);
+
+    return 0;
+}
